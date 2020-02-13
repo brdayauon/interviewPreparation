8836e5e08c20c397b8c521b5372b51406f54f1db
brdayauon <brdayauon@gmail,com>
2020-02-13 12:51:34 -0800

    fiirst pyFile

---
+++ detectLLCycle.py
@@ -0,0 +1,32 @@
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution(object):
+  def hasCycle(self, head):
+    # Fill this in.
+    slow = head
+    fast = head
+    
+    while fast and fast.next:
+        slow = slow.next 
+        fast = fast.next.next 
+
+        if slow == fast:
+            return True
+    return False
+
+testHead = ListNode(4)
+node1 = ListNode(3)
+testHead.next = node1
+node2 = ListNode(2)
+node1.next = node2
+node3 = ListNode(1)
+node2.next = node3
+testTail = ListNode(0)
+node3.next = testTail
+testTail.next = node1
+
+print(Solution().hasCycle(testHead))
+# True
