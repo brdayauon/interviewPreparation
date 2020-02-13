13331e0bbd1019f4a8912b03218277dee3ca326e
brdayauon <brdayauon@gmail,com>
2020-02-13 15:15:00 -0800

    uniquePaths DP

---
+++ uniquePaths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        
+        dp = [1] * n
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j] += dp[j-1]
+        
+        return dp[n-1]
